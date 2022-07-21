61b8f6eb5385675036cbfb27afe16d178bedda83
neha06-git <nehaguta2000@gmail.com>
2022-07-21 15:04:57 +0530

    Adding First small project of React
    Create a first hello world app and using CSS glassmorphism effect in it.

---
+++ First/helloworld-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import avtarImage from './avtar.jpg';
+function App() {
+  return (
+    <div>
+      <h2>Hello World Glassmorphism Effect</h2>
+    <div className="container">
+      <img src={avtarImage} alt='Avtar' />
+      <div className="card">
+       <h4>John Doe</h4>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea magnam aspernatur quo cumque rerum, molestias ipsum obcaecati laborum voluptas adipisci ipsa maxime modi mollitia dolore temporibus facilis, consequuntur illum ab.</p>
+      
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
